a00c8a5f675ceeeb574c26996ac57b45d7ebda4e
Nitish <nitishm280@gmail.com>
2023-06-23 19:06:33 +0530

    create navigation bar

---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import ImageSlider from "./ImageSlider";
+
+const Home = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <>
+        <ImageSlider />
+      </>
+    </>
+  );
+};
+
+export default Home;
